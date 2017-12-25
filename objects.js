172d56f46300ec9b9d1dd1e2e15bdae90aa8dbee
Tim Hillhouse <thillhouse2@gmail.com>
2017-12-25 17:48:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,4 +7,5 @@ function updatePlaylist(playlist, artistName, songTitle){
 function removeFromPlaylist(playlist, artistName){
   console.log(playlist);
   delete playlist.artistName;
+  console.log(playlist);
 }
